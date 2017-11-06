eb1ed90da62cc60b6a65756c8311de9e13a1dc88
update gradle <egidijusgecius@Egidijuss-MBP.default>
2017-11-06 15:46:28 +0000

    add example how test code does not compile from another modules 'test' code

--- app/src/test/java/com/example/egidijusgecius/demo_testingmodule/ExampleUnitTest.java
+++ app/src/test/java/com/example/egidijusgecius/demo_testingmodule/ExampleUnitTest.java
@@ -1,16 +1,26 @@
 package com.example.egidijusgecius.demo_testingmodule;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Ignore // this does not compile because class is in another modules' test code
+    @Test
+    public void getDataFromLibrary() {
+//        String string = TestDataProviderLibary.getString();
+//        assertEquals(string, "s");
+
     }
 }
---
+++ mylibrary/src/test/java/com/example/mylibrary/TestDataProviderLibary.java
@@ -0,0 +1,8 @@
+package com.example.mylibrary;
+
+public class TestDataProviderLibary {
+
+    public static String getString() {
+        return "string";
+    }
+}
